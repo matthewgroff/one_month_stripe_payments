73eef984142a37d4f3f7122b08ff0c7a41cc2f71
Matt Groff <matthewjgroff@gmail.com>
2016-02-06 18:21:08 -0800

    Added the ability to store purchases in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204053911) do
+ActiveRecord::Schema.define(version: 20160207021539) do
 
   create_table "purchases", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.integer  "amount"
+    t.string   "description"
+    t.string   "currency"
+    t.string   "customer_id"
+    t.string   "card"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
